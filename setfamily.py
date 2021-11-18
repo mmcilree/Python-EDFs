c2a12f6b5a34f003f93dd5b5881af5c04a6fcd89
mmcilree <mjm42@st-andrews.ac.uk>
2021-11-18 11:53:41 +0000

    Replaced edftools with OO set family design.

--- setfamily.py
+++ setfamily.py
@@ -1,4 +1,5 @@
 from utils import all_equal
+import itertools
 
 
 class SetFamily:
@@ -62,7 +63,7 @@ class SetFamily:
         all_counts = []
         for i in range(self.m):
             for j in range(self.m):
-                if i != j :
+                if i != j:
                     ext_diffs = self.external_differences(i, j)
 
                     counts = [
@@ -74,6 +75,94 @@ class SetFamily:
 
         return all_equal(all_counts)
 
+    def el_as_str(self, el):
+        """String representation of element: may be overridden in subclasses."""
+        return str(el)
+
+    def print_external_differences(
+        self, sort_by=None, headings=None, latex=True, perline=5
+    ):
+        """Pretty print all external differences."""
+        ed = {}
+        for i in range(self.m):
+            for j in range(self.m):
+                if i != j:
+                    ed.update(self.external_differences(i, j))
+
+        if sort_by == "value":
+            ed = dict(sorted(ed.items(), key=lambda item: item[1]))
+
+        count = 0
+        for diff in ed.keys():
+
+            print(
+                ("& " if latex else " ")
+                + self.el_as_str(diff[0])
+                + " - "
+                + self.el_as_str(diff[1])
+                + " = "
+                + self.el_as_str(ed[diff]),
+                end=" ",
+            )
+            count += 1
+            if count % perline == 0:
+                print("\\\\" if latex else "")
+        print()
+
+
+class CyclicSetFamily(SetFamily):
+    """Represent a family of sets with elements from the cyclic group Z_n"""
+    def __init__(self, sets, mod):
+        group_table = [[(i + j) % mod for i in range(mod)] for j in range(mod)]
+        inverses = [(-i) % mod for i in range(mod)]
+        super(CyclicSetFamily, self).__init__(
+            sets, group_table, inverses=inverses
+        )
+
+
+class CyclicProductSetFamily(SetFamily):
+    """
+        Represent a family of sets with elements from a direct product of cyclic
+        groups Z_a1 x Z_a2 x ... Z_ak
+    """
+    def __init__(self, sets, mod):
+        """Expect sets of tuples for initialisation."""
+        if type(mod) is not list:
+            # If a single integer is given as mod, assume all groups in the 
+            # product have that size. 
+            mod = [mod for _ in range(len(sets[0][0]))]
+
+        groups = [range(m) for m in mod]
+        elements = list(itertools.product(*groups))
+        sets = [list(map(elements.index, s)) for s in sets]
+
+        group_table = [
+            [
+                elements.index(
+                    tuple(
+                        [
+                            (elements[i][k] + elements[j][k]) % mod[k]
+                            for k in range(len(mod))
+                        ]
+                    )
+                )
+                for i in range(len(elements))
+            ]
+            for j in range(len(elements))
+        ]
+
+        inverses = [
+            elements.index(tuple([-elements[i][k] % mod[k] for k in range(len(mod))]))
+            for i in range(len(elements))
+        ]
+
+        super(CyclicProductSetFamily, self).__init__(
+            sets, group_table, inverses=inverses
+        )
+
+    def el_as_str(self, el):
+        return str(self.elements[el])
+
 
 if __name__ == "__main__":
     mod_5_mult = [
@@ -84,9 +173,21 @@ if __name__ == "__main__":
         [4, 0, 1, 2, 3],
     ]
 
-    test = SetFamily([[0, 1], [2, 4]], mod_5_mult)
+    test = CyclicSetFamily([[0, 1], [2, 4]], 5)
     print(test.mult(2, 3))
     print(test.diff(4, 1))
     print(test.diff(2, 3))
     print(test.is_edf())  # True
-    print(test.is_sedf()) # True
+    print(test.is_sedf())  # True
+    test.print_external_differences(sort_by="value")
+
+    test2 = CyclicProductSetFamily(
+        [
+            [(1, 2), (2, 1), (2, 2), (2, 3), (3, 2)],
+            [(0, 1), (0, 3), (1, 3), (2, 0), (3, 1)],
+            [(0, 2), (1, 0), (1, 1), (3, 0), (3, 3)],
+        ],
+        4,
+    )
+    print(test2.is_edf())  # True
+    print(test2.is_sedf())  # False
--- test.py
+++ test.py
@@ -1,27 +1,34 @@
-from edftools import *
+from setfamily import *
 from testedfs import *
 
+t1 = CyclicSetFamily(p_2_1, 19)
 print(str(p_2_1) + ":")
-#print_ed(p_2_1, 19)
-print("Is edf? " + str(is_edf(p_2_1, 19)))
-print("Is strong edf? " + str(is_sedf(p_2_1, 19)))
+
+print("Is edf? {0}".format(t1.is_edf()))
+print("Is strong edf? {0}".format(t1.is_sedf()))
 print("----")
 
+t2 = CyclicSetFamily(non_edf_1, 31)
 print(str(non_edf_1) + ":")
-#print_ed(non_edf_1, 31)
-print("Is edf? " + str(is_edf(p_2_1, 31)))
-print("Is strong edf? " + str(is_sedf(p_2_1, 31)))
+print("Is edf? {0}".format(t1.is_edf()))
+print("Is strong edf? {0}".format(t1.is_sedf()))
 print("----")
 
 for h in h_7_all_with_mods:
     h_sedf = h[0]
     h_mod = h[1]
+    if type(h_sedf[0][0]) is tuple:
+        th = CyclicProductSetFamily(h_sedf, h_mod)
+    else:
+        th = CyclicSetFamily(h_sedf, h_mod)
+
     print(str(h_sedf) + ":")
-    print("Is edf? " + str(is_edf(h_sedf, h_mod)))
-    print("Is strong edf? " + str(is_sedf(h_sedf, h_mod)))
+    print("Is edf? {0}".format(th.is_edf()))
+    print("Is strong edf? {0}".format(th.is_sedf()))
     print("----")
 
+t3 = CyclicProductSetFamily(d_3_1, 4)
 print(str(d_3_1) + ":")
-print("Is edf? " + str(is_edf(d_3_1, 4)))
-print("Is strong edf? " + str(is_sedf(d_3_1, 4)))
+print("Is edf? {0}".format(t3.is_edf()))
+print("Is strong edf? {0}".format(t3.is_sedf()))
 print("----")
